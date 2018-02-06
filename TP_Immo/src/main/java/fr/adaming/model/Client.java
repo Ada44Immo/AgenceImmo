e5976ced2b9c61412d0ed68e407e187985d5667b
Adaming <Adaming@192.168.1.168>
2018-02-06 15:38:56 +0000

    anais: commit des classes model achat,location,visite et client

--- TP_Immo/src/main/java/fr/adaming/model/Client.java
+++ TP_Immo/src/main/java/fr/adaming/model/Client.java
@@ -1,9 +1,85 @@
 package fr.adaming.model;
 
-public class Client {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clients")
+public class Client extends Personne{
+
+	//attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)	
+	int id;
+	
+	//relation UML en JAVA	
+	//relation client/visite
+	@OneToMany(mappedBy="client")
+	private List<Visite> listeVisite;
+			
+	//relation client/classe standard
+	@ManyToMany
+	@JoinTable(name="client_classestd",joinColumns=@JoinColumn(name="visite_id"),inverseJoinColumns=@JoinColumn(name="client_id"))
+	private List<ClasseStandard> listeCs;
+			
+	//constructeur
 	public Client() {
-		// TODO Aut  o-generated constructor stub
+		super();
 	}
+
+		
+	public Client(Adresse adresse, int tel, String nom) {
+		super(adresse, tel, nom);
+	}
+
+
+
+	public Client(Adresse adresse, int tel, String nom, int id) {
+		super(adresse, tel, nom);
+		this.id = id;
+	}
+
+
+
+	//getter et setter
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<Visite> getListeVisite() {
+		return listeVisite;
+	}
+
+	public void setListeVisite(List<Visite> listeVisite) {
+		this.listeVisite = listeVisite;
+	}
+
+	public List<ClasseStandard> getListeCs() {
+		return listeCs;
+	}
+
+	public void setListeCs(List<ClasseStandard> listeCs) {
+		this.listeCs = listeCs;
+	}
+	
+	
+	
+	
 
 }
---
+++ TP_Immo/src/main/java/fr/adaming/model/Visite.java
@@ -0,0 +1,106 @@
+package fr.adaming.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="visites")
+public class Visite {
+	//attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int id;
+	@Temporal(TemporalType.DATE)
+	Date date;
+	
+	//relation UML en JAVA
+	//relation client/visite
+	@ManyToOne
+	@JoinColumn(name="c_id",referencedColumnName="id")
+	private Client client;
+	
+	//relation visite/bien
+	@ManyToOne
+	private List<Location> listeLocation;
+	
+	@ManyToOne
+	private List<Achat> listeAchat;
+	
+	//relation visite/agent
+	@ManyToOne
+	@JoinColumn(name="a_id",referencedColumnName="id")
+	private Agent agent;
+	
+	//constructeur
+	public Visite() {
+		super();
+	}
+
+
+	public Visite(Date date) {
+		super();
+		this.date = date;
+	}
+
+
+	public Visite(int id, Date date) {
+		super();
+		this.id = id;
+		this.date = date;
+	}
+
+	//getter et setter
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+	public Client getClient() {
+		return client;
+	}
+
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+
+	public Agent getAgent() {
+		return agent;
+	}
+
+
+	public void setAgent(Agent agent) {
+		this.agent = agent;
+	}
+	
+	
+	
+}
