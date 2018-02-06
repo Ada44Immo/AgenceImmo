307bde9f551dd347f29b0381e67ae5ffbcad38ec
intiformation <intiformation@192.168.1.93>
2018-02-06 15:14:09 +0100

    Guillaume : classe propietaire fin

--- TP_Immo/src/main/java/fr/adaming/model/Proprietaire.java
+++ TP_Immo/src/main/java/fr/adaming/model/Proprietaire.java
@@ -7,6 +7,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
+/**
+ * La classe Propietaire herite de la classe personne et ajoute comme attribut un entier id. 
+ * C'est une classe persistante. 
+ * 
+ * @author intiformation
+ *
+ */
 @Entity
 @Table(name="proprietaires")
 public class Proprietaire extends Personne {
@@ -15,6 +23,31 @@ public class Proprietaire extends Personne {
 	@Column(name="id_p")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	
+
+	public Proprietaire() {
+		super();
+	}
+
+	public Proprietaire(Adresse adresse, int tel, String nom) {
+		super(adresse, tel, nom);
+	}
+
+	public Proprietaire(Adresse adresse, int tel, String nom, int id) {
+		super(adresse, tel, nom);
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Proprietaire [id=" + id + "]";
+	}
 	
 }
