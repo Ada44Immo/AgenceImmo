164c361fae6227a2242a6ec5625b636bd9c8b4d5
inti0294 <inti0294@192.168.1.23>
2018-02-06 16:53:56 +0100

    manu

--- TP_Immo/src/main/java/fr/adaming/model/Achat.java
+++ TP_Immo/src/main/java/fr/adaming/model/Achat.java
@@ -1,11 +1,80 @@
 package fr.adaming.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-public class Achat {
-
+import javax.persistence.Table;
+@Entity
+@Table(name="achats")
+public class Achat extends Bien{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private double prix;
+	private String etat;
 	
+	public Achat() {
+		super();
+	}
+	
+	
+	
+
+	public Achat(String statut, Date dateDispo, Date dateMiseEnGerance, Adresse adresse, String description,
+			double revenuCasdastral, double surface, byte[] photo, double prix, String etat,
+			Proprietaire proprietaire) {
+		super(statut, dateDispo, dateMiseEnGerance, adresse, description, revenuCasdastral, surface, photo);
+		this.prix = prix;
+		this.etat = etat;
+		this.proprietaire = proprietaire;
+	}
+
+
+
+
+	public Achat(String statut, Date dateDispo, Date dateMiseEnGerance, Adresse adresse, String description,
+			double revenuCasdastral, double surface, byte[] photo, int id, double prix, String etat,
+			Proprietaire proprietaire) {
+		super(statut, dateDispo, dateMiseEnGerance, adresse, description, revenuCasdastral, surface, photo);
+		this.id = id;
+		this.prix = prix;
+		this.etat = etat;
+		this.proprietaire = proprietaire;
+	}
+	
+	
+
+
+
+
+	public Achat(String statut, Date dateDispo, Date dateMiseEnGerance, Adresse adresse, String description,
+			double revenuCasdastral, double surface, int id, double prix, String etat, Proprietaire proprietaire) {
+		super(statut, dateDispo, dateMiseEnGerance, adresse, description, revenuCasdastral, surface);
+		this.id = id;
+		this.prix = prix;
+		this.etat = etat;
+		this.proprietaire = proprietaire;
+	}
+
+
+	public Achat(String statut, Date dateDispo, Date dateMiseEnGerance, Adresse adresse, String description,
+			double revenuCasdastral, double surface, double prix, String etat, Proprietaire proprietaire) {
+		super(statut, dateDispo, dateMiseEnGerance, adresse, description, revenuCasdastral, surface);
+		this.prix = prix;
+		this.etat = etat;
+		this.proprietaire = proprietaire;
+	}
+
+
+
+
+
+
 	// Transformation des associations UML en java
 	@ManyToOne
 	@JoinColumn(name="proprietaire_id", referencedColumnName="id_p")
--- TP_Immo/src/main/java/fr/adaming/model/Location.java
+++ TP_Immo/src/main/java/fr/adaming/model/Location.java
@@ -1,11 +1,147 @@
 package fr.adaming.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
-public class Location {
+@Entity
+@Table(name="locations")
+public class Location extends Bien {
 
-	// Transformation des associations UML en java
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private double caution;
+	private double loyer;
+	private double charge;
+	private String bail;
+	private boolean garniture;
+	
+
+	public Location() {
+		super();
+	}
+	
+	
+	
+
+	public Location(String statut, Date dateDispo, Date dateMiseEnGerance, Adresse adresse, String description,
+			double revenuCasdastral, double surface, byte[] photo, double caution, double loyer, double charge,
+			String bail, boolean garniture, Proprietaire proprietaire) {
+		super(statut, dateDispo, dateMiseEnGerance, adresse, description, revenuCasdastral, surface, photo);
+		this.caution = caution;
+		this.loyer = loyer;
+		this.charge = charge;
+		this.bail = bail;
+		this.garniture = garniture;
+		this.proprietaire = proprietaire;
+	}
+
+	public Location(String statut, Date dateDispo, Date dateMiseEnGerance, Adresse adresse, String description,
+			double revenuCasdastral, double surface, double caution, double loyer, double charge, String bail,
+			boolean garniture, Proprietaire proprietaire) {
+		super(statut, dateDispo, dateMiseEnGerance, adresse, description, revenuCasdastral, surface);
+		this.caution = caution;
+		this.loyer = loyer;
+		this.charge = charge;
+		this.bail = bail;
+		this.garniture = garniture;
+		this.proprietaire = proprietaire;
+	}
+
+	
+
+	public Location(String statut, Date dateDispo, Date dateMiseEnGerance, Adresse adresse, String description,
+			double revenuCasdastral, double surface, int id, double caution, double loyer, double charge, String bail,
+			boolean garniture, Proprietaire proprietaire) {
+		super(statut, dateDispo, dateMiseEnGerance, adresse, description, revenuCasdastral, surface);
+		this.id = id;
+		this.caution = caution;
+		this.loyer = loyer;
+		this.charge = charge;
+		this.bail = bail;
+		this.garniture = garniture;
+		this.proprietaire = proprietaire;
+	}
+
+
+
+
+	public Location(String statut, Date dateDispo, Date dateMiseEnGerance, Adresse adresse, String description,
+			double revenuCasdastral, double surface, byte[] photo, int id, double caution, double loyer, double charge,
+			String bail, boolean garniture, Proprietaire proprietaire) {
+		super(statut, dateDispo, dateMiseEnGerance, adresse, description, revenuCasdastral, surface, photo);
+		this.id = id;
+		this.caution = caution;
+		this.loyer = loyer;
+		this.charge = charge;
+		this.bail = bail;
+		this.garniture = garniture;
+		this.proprietaire = proprietaire;
+	}
+
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getCaution() {
+		return caution;
+	}
+
+	public void setCaution(double caution) {
+		this.caution = caution;
+	}
+
+	public double getLoyer() {
+		return loyer;
+	}
+
+	public void setLoyer(double loyer) {
+		this.loyer = loyer;
+	}
+
+	public double getCharge() {
+		return charge;
+	}
+
+	public void setCharge(double charge) {
+		this.charge = charge;
+	}
+
+	public String getBail() {
+		return bail;
+	}
+
+	public void setBail(String bail) {
+		this.bail = bail;
+	}
+
+	public boolean isGarniture() {
+		return garniture;
+	}
+
+	public void setGarniture(boolean garniture) {
+		this.garniture = garniture;
+	}
+
+
+
+
+		// Transformation des associations UML en java
 		@ManyToOne
 		@JoinColumn(name="proprietaire_id", referencedColumnName="id_p")
 		private Proprietaire proprietaire;
@@ -17,6 +153,8 @@ public class Location {
 		public void setProprietaire(Proprietaire proprietaire) {
 			this.proprietaire = proprietaire;
 		}
+		
+		
 	
 		
 }
