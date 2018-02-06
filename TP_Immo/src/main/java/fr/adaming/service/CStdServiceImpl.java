7f45638d5fdc368241983373d49740d508f9330a
Adaming44 <Adaming44@192.168.1.148>
2018-02-06 16:02:21 +0100

    lara update

--- TP_Immo/src/main/java/fr/adaming/service/CStdServiceImpl.java
+++ TP_Immo/src/main/java/fr/adaming/service/CStdServiceImpl.java
@@ -2,11 +2,13 @@ package fr.adaming.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.adaming.dao.ICStdDao;
 import fr.adaming.model.ClasseStandard;
 
 @Service
+@Transactional
 public class CStdServiceImpl implements ICStdService {
 	
 	@Autowired
