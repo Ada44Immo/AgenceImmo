2528fb96832c5c5a0e2d147046bbd6f9120b04c1
Adaming44 <Adaming44@192.168.1.148>
2018-02-06 15:15:16 +0100

    lara : classe standard model et dao

---
+++ TP_Immo/src/main/java/fr/adaming/dao/ICStdDao.java
@@ -0,0 +1,9 @@
+package fr.adaming.dao;
+
+import fr.adaming.model.ClasseStandard;
+
+public interface ICStdDao {
+	
+	public ClasseStandard addCStd(ClasseStandard CStd);
+
+}
--- TP_Immo/src/main/java/fr/adaming/model/ClasseStandard.java
+++ TP_Immo/src/main/java/fr/adaming/model/ClasseStandard.java
@@ -1,9 +1,93 @@
 package fr.adaming.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="classestandard")
 public class ClasseStandard {
 
 	//les attributs de la classe
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idCode;
+	private String type;
+	private boolean modeOffre;
+	private double prixMax;
+	private double surfaceMin;
+	
+	//constructeur vide
+	public ClasseStandard() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	//constructeur avec arguments
+	public ClasseStandard(String type, boolean modeOffre, double prixMax, double surfaceMin) {
+		super();
+		this.type = type;
+		this.modeOffre = modeOffre;
+		this.prixMax = prixMax;
+		this.surfaceMin = surfaceMin;
+	}
+
+	public ClasseStandard(int idCode, String type, boolean modeOffre, double prixMax, double surfaceMin) {
+		super();
+		this.idCode = idCode;
+		this.type = type;
+		this.modeOffre = modeOffre;
+		this.prixMax = prixMax;
+		this.surfaceMin = surfaceMin;
+	}
+
+	//les getters et setters
+	public int getIdCode() {
+		return idCode;
+	}
+
+	public void setIdCode(int idCode) {
+		this.idCode = idCode;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public boolean isModeOffre() {
+		return modeOffre;
+	}
+
+	public void setModeOffre(boolean modeOffre) {
+		this.modeOffre = modeOffre;
+	}
+
+	public double getPrixMax() {
+		return prixMax;
+	}
+
+	public void setPrixMax(double prixMax) {
+		this.prixMax = prixMax;
+	}
+
+	public double getSurfaceMin() {
+		return surfaceMin;
+	}
+
+	public void setSurfaceMin(double surfaceMin) {
+		this.surfaceMin = surfaceMin;
+	}	
+	
+	
+	
+	
+	
 	
 	
 }
