69560b7233cdc5a49ddfba3d6d3e798b0b30e0af
Adaming <Adaming@192.168.1.168>
2018-02-07 11:47:45 +0000

    anais: commit client test + rest

---
+++ TP_Immo/src/main/java/fr/adaming/rest/ClientRestController.java
@@ -0,0 +1,55 @@
+package fr.adaming.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.adaming.model.Client;
+import fr.adaming.service.IClientService;
+
+@RestController
+@RequestMapping(value="client")
+public class ClientRestController {
+	@Autowired
+	IClientService clientService;
+
+	public void setClientService(IClientService clientService) {
+		this.clientService = clientService;
+	}
+	
+	@RequestMapping(value="liste",method=RequestMethod.GET,produces="application/json")
+	public List<Client> getAll(){
+		
+		return clientService.getAllClient();
+	}
+	
+	@RequestMapping(value="ByNom",method=RequestMethod.GET,produces="application/json")
+	public Client getByNom(@RequestParam("pNom") String nom){
+		
+		return clientService.getByNom(nom);
+	}
+	
+	@RequestMapping(value="delete",method=RequestMethod.DELETE,consumes="application/json")
+	public int deleteClient(@RequestBody Client c){
+		
+		return clientService.deleteClient(c);
+	}
+	
+	@RequestMapping(value="update",method=RequestMethod.PUT,consumes="application/json",produces="application/json")
+	public Client updateCLient(@RequestBody Client client){
+		
+		return clientService.updateClient(client);
+	}
+	
+	@RequestMapping(value="add",method=RequestMethod.POST,consumes="application/json",produces="application/json")
+	public Client addCLient(@RequestBody Client client){
+		
+		return clientService.addClient(client);
+	}
+	
+}
--- TP_Immo/src/test/java/fr/adaming/testDao/ClientDaoImplTest.java
+++ TP_Immo/src/test/java/fr/adaming/testDao/ClientDaoImplTest.java
@@ -1,61 +1,89 @@
-//package fr.adaming.testDao;
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.annotation.Rollback;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import fr.adaming.dao.IClientDao;
-//import fr.adaming.model.Adresse;
-//import fr.adaming.model.Client;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/application-context.xml" })
-//@Transactional
-//public class ClientDaoImplTest {
-//	
-//	//test des methodes du Dao de Client
-//	@Autowired
-//	IClientDao clientDao;
-//	
-//	Adresse adresse=new Adresse("rue Crebillon", "24", 44000, "Nantes", "France");		
-//	Client client=new Client(adresse, 29, "TOTO");
-//	
-//	//methode getAllCLient
-//	@Test
-//	@Transactional(readOnly=true)
-//	public void getAllTest(){
-//		
-//		assertNotNull(clientDao.getAllClient());
-//		
-//		
-//	}
-//	
-//	
-//	//test methode ajouter
-////	@Test
-////	@Rollback(true)
-////	@Transactional
-////	public void addClientTest(){
-////		int tailleAvant=clientDao.getAllClient().size();
-////		clientDao.addClient(client);
-////		assertEquals(tailleAvant+1,clientDao.getAllClient().size());
-////	}
-//	
-//	//methode modifier
-////	public void updateTest(){
-////		client.setNom("FOFO");
-////		Client cModif=clientDao.updateClient(client);
-////		
-////		assertEquals("FOFO", cModif.getNom());
-////		
-////	}
-//	
-//	
-//	
-//}
+package fr.adaming.testDao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.adaming.dao.IClientDao;
+import fr.adaming.model.Adresse;
+import fr.adaming.model.Client;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/application-context.xml" })
+@Transactional
+public class ClientDaoImplTest {
+	
+	//test des methodes du Dao de Client
+	@Autowired
+	IClientDao clientDao;
+	
+	Adresse adresse=new Adresse("rue Crebillon", "24", 44000, "Nantes", "France");		
+	Client client=new Client(adresse, 29, "TOTO");
+	Client client2=new Client(adresse, 40, "FOFO",1);
+	
+	//methode getAllCLient
+	@Ignore
+	@Test
+	@Transactional(readOnly=true)
+	public void getAllTest(){
+		
+		assertNotNull(clientDao.getAllClient());
+		
+		
+	}
+	 //getbyNom
+	@Ignore
+	@Test
+	@Transactional(readOnly=true)
+	public void getByNomTest(){
+		Client c1=clientDao.getByNom("TOTO");
+		assertEquals("Nantes", c1.getAdresse().getLocalite());
+	}
+	
+	
+	//test methode ajouter
+	@Ignore
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void addClientTest(){
+		int tailleAvant=clientDao.getAllClient().size();
+		clientDao.addClient(client);
+		assertEquals(tailleAvant+1,clientDao.getAllClient().size());
+	}
+	
+	//methode modifier
+	@Ignore
+	@Test
+	@Rollback(true)
+	@Transactional
+	public void updateTest(){
+		Client c2=clientDao.getByNom("TOTO");
+		c2.setNom("TITI");
+		Client cModif=clientDao.updateClient(c2);
+		
+		assertEquals("TITI", cModif.getNom());
+		
+	}
+	
+	//test delete
+	@Ignore
+	@Test
+	@Rollback(true)
+	@Transactional
+	 public void deleteTest(){
+		 int tailleAvant=clientDao.getAllClient().size();
+		 Client c3=clientDao.getByNom("TOTO");
+		 int verif=clientDao.deleteClient(c3);
+		 assertEquals(1, verif);
+		 
+	 }
+	
+}
