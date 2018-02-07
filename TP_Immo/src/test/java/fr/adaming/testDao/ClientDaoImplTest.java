f20d7737e36a916730fd2d840eced02a7a1b082b
Adaming <Adaming@192.168.1.168>
2018-02-07 10:15:03 +0000

    anais: commit client test

---
+++ TP_Immo/src/test/java/fr/adaming/testDao/ClientDaoImplTest.java
@@ -0,0 +1,22 @@
+package fr.adaming.testDao;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.adaming.dao.IClientDao;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/application-context.xml" })
+@Transactional
+public class ClientDaoImplTest {
+	
+	//test des methodes du Dao de Client
+	@Autowired
+	IClientDao clientDao;
+	
+	
+
+}
